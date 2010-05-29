a3c0ae46a65126fc8db756960a1b9511942600d8
Sarah Spikes <sarah@udacity.com>
2010-05-28 20:05:14 -0700

    Create helper functions

--- game.js
+++ game.js
@@ -383,6 +383,25 @@ Ship = function () {
 
   this.collidesWith = ["asteroid", "bigalien", "alienbullet", "coin"];
 
+  this.shoot = function() {
+    for (var i = 0; i < this.bullets.length; i++) {
+      if (!this.bullets[i].visible) {
+        SFX.laser();
+        var bullet = this.bullets[i];
+        var rad = ((this.rot-90) * Math.PI)/180;
+        var vectorx = Math.cos(rad);
+        var vectory = Math.sin(rad);
+        // move to the nose of the ship
+        bullet.x = this.x + vectorx * 4;
+        bullet.y = this.y + vectory * 4;
+        bullet.vel.x = 6 * vectorx + this.vel.x;
+        bullet.vel.y = 6 * vectory + this.vel.y;
+        bullet.visible = true;
+        break;
+      }
+    }
+  }
+
   this.preMove = function (delta) {
     if (KEY_STATUS.left) {
       this.vel.rot = -6;
@@ -409,22 +428,7 @@ Ship = function () {
     if (KEY_STATUS.space) {
       if (this.delayBeforeBullet <= 0) {
         this.delayBeforeBullet = 10;
-        for (var i = 0; i < this.bullets.length; i++) {
-          if (!this.bullets[i].visible) {
-            SFX.laser();
-            var bullet = this.bullets[i];
-            var rad = ((this.rot-90) * Math.PI)/180;
-            var vectorx = Math.cos(rad);
-            var vectory = Math.sin(rad);
-            // move to the nose of the ship
-            bullet.x = this.x + vectorx * 4;
-            bullet.y = this.y + vectory * 4;
-            bullet.vel.x = 6 * vectorx + this.vel.x;
-            bullet.vel.y = 6 * vectory + this.vel.y;
-            bullet.visible = true;
-            break;
-          }
-        }
+        this.shoot();
       }
     }
 
@@ -656,23 +660,26 @@ Asteroid = function () {
 
   this.collidesWith = ["ship", "bullet", "bigalien", "alienbullet"];
 
+  this.breakIntoFragments = function () {
+    for (var i = 0; i < 3; i++) {
+      var roid = $.extend(true, {}, this);
+      roid.vel.x = Math.random() * 6 - 3;
+      roid.vel.y = Math.random() * 6 - 3;
+      if (Math.random() > 0.5) {
+        roid.points.reverse();
+      }
+      roid.vel.rot = Math.random() * 2 - 1;
+      roid.move(roid.scale * 3); // give them a little push
+      Game.sprites.push(roid);
+    }
+  }
+
   this.collision = function (other) {
     SFX.explosion();
     if (other.name == "bullet") Game.score += 120 / this.scale;
     this.scale /= 3;
     if (this.scale > 0.5) {
-      // break into fragments
-      for (var i = 0; i < 3; i++) {
-        var roid = $.extend(true, {}, this);
-        roid.vel.x = Math.random() * 6 - 3;
-        roid.vel.y = Math.random() * 6 - 3;
-        if (Math.random() > 0.5) {
-          roid.points.reverse();
-        }
-        roid.vel.rot = Math.random() * 2 - 1;
-        roid.move(roid.scale * 3); // give them a little push
-        Game.sprites.push(roid);
-      }
+      this.breakIntoFragments();
     }
     Game.explosionAt(other.x, other.y);
     this.die();
